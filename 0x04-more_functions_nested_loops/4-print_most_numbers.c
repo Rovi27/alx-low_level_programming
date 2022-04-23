758f3789b4875c5ab4bc67d4fa5cb285ba9b4306
Rovi27 <yeshuaa27@gmail.com>
2022-04-23 03:54:00 -0700

    print_most_numbers - prints 01356789\n

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -17,23 +17,4 @@ void print_most_numbers(void)
 			_putchar(c);
 	}
 	_putchar('\n');
-}#include "main.h"
-
-/**
- * print_most_numbers - prints 01356789\n
- *
- * Return: void
- */
-
-
-void print_most_numbers(void)
-{
-	char c;
-
-	for (c = '0'; c <= '9'; c++)
-	{
-		if (!(c == '2' || c == '4'))
-			_putchar(c);
-	}
-	_putchar('\n');
 }
